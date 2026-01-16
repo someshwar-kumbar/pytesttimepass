e4352c0ab6f55c053adbaa70ed8220bc95553e52
someshwar-kumbar <tncxsomu@gmail.com>
2026-01-17 01:43:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 from pro2 import maxnumber
-import pytest
+
 def test_maxnumber():
     assert maxnumber(10, 20, 30) == 30
     assert maxnumber(-10, -20, -5) == -5
     assert maxnumber(0, 0, 0) == 0
     assert maxnumber(1.5, 2.5, 0.5) == 2.5
-test_maxnumber()
-
-    
